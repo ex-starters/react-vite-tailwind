d3d0e4948ece299c3846ad791b5da382702006b2
Ahmed <aahmed@mum.edu>
2022-12-22 13:10:00 -0500

    adding @fortawesome/fontawesome-free: ^6.2.1,

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import {useState} from 'react'
 import './App.scss'
 
 function App() {
-    const [count, setCount] = useState(0)
-
-    return <h1 className="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    return <header className='flex justify-between'>
+        <div className="logo flex">
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-square-plus" width="24"
+                 height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                 stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <rect x="4" y="4" width="16" height="16" rx="2"></rect>
+                <line x1="9" y1="12" x2="15" y2="12"></line>
+                <line x1="12" y1="9" x2="12" y2="15"></line>
+            </svg>
+            Informatica
+        </div>
+        <nav className='flex gap-4'>
+            <a href="#">product</a>
+            <a href="#">About</a>
+            <a href="#">BLog</a>
+            <a href="#">Career</a>
+        </nav>
+    </header>
 
 }
 
